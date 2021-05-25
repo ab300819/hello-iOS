cf51898a15a7e02655186c1c6f331fc078bc8c04
Mason <ab300819@163.com>
2021-05-25 23:02:27 +0800

    08.视图控制 UIViewController 生命周期

--- HelloIOS/ViewController.m
+++ HelloIOS/ViewController.m
@@ -7,25 +7,81 @@
 
 #import "ViewController.h"
 
+@interface TestView : UIView
+
+@end
+
+@implementation TestView
+
+-(instancetype)init{
+    NSLog(@"TestView init");
+    self=[super init];
+    if(self){
+        
+    }
+    
+    return self;
+}
+
+-(void)willMoveToSuperview:(UIView *)newSuperview{
+    NSLog(@"willMoveToSuperview init");
+    [super willMoveToSuperview:newSuperview];
+}
+
+-(void)didMoveToSuperview{
+    NSLog(@"didMoveToSuperview");
+    [super didMoveToSuperview];
+}
+
+-(void)willMoveToWindow:(UIWindow *)newWindow{
+    NSLog(@"willMoveToWindow");
+    [super willMoveToWindow:newWindow];
+}
+
+@end
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
+-(instancetype)init{
+    NSLog(@"ViewConntroller init");
+    self=[super init];
+    if(self){
+        
+    }
+    return self;
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    NSLog(@"viewWillAppear init");
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    NSLog(@"viewDidAppear init");
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    NSLog(@"viewWillDisappear init");
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    NSLog(@"viewDidDisappear init");
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    UIView *view=[[UIView alloc]init];
-    view.backgroundColor=[UIColor redColor];
+    TestView *view=[[TestView alloc]init];
+    view.backgroundColor=[UIColor greenColor];
     view.frame=CGRectMake(100, 100, 100, 100);
     [self.view addSubview:view];
-    
-    UIView *view2=[[UIView alloc]init];
-    view2.backgroundColor=[UIColor blueColor];
-    view2.frame=CGRectMake(100, 200, 200, 100);
-    [self.view addSubview:view2];
 }
 
 
